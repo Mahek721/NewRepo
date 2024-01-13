478114209d5e25fd7ea10485919ddec1e7aa2c7a
Mahek721 <mahekshobhasana@yahoo.com>
2024-01-13 08:15:18 +0530

    Added my project

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Elite',
+      title: 'Elite Ecommerce',
       debugShowCheckedModeBanner: false,
       home: MainScreen(),
     );
